2fd98c0fb5160d51bf845dc73e4be6eef62b67d5
Thiago Nahum <nahumm01@outlook.com>
2023-08-10 20:35:09 -0300

    :tada: Iniciando repo Vercel

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $("header button").click(function () {
+    $("form").slideDown();
+  });
+
+  $("#btnCancel").click(function () {
+    $("form").slideUp();
+  });
+
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+
+    const endNovaImg = $("#endImgNova").val();
+    const novoItem = $('<li style="display: none"> </li>');
+    $(`<img src="${endNovaImg}">`).appendTo(novoItem);
+    $(`<div class="overlayImg">
+      <a href="${endNovaImg}" target= "_blank" title="Ver imagem em tamanho real">
+        Ver imagem em tamanho real
+      </a>
+    </div>
+    `).appendTo(novoItem);
+    $(novoItem).appendTo("ul");
+    $(novoItem).fadeIn(5000);
+    $("#endImgNova").val("")
+  });
+});
